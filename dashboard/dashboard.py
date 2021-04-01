46a97b4be0d9af44d6090f27e61f177d79eeff0e
Robert Andersson <robert.m.andersson@atlascopco.com>
2021-04-01 14:18:41 +0200

    Rework API and add deduplication support
    Signed-off-by: Robert Andersson <robert.m.andersson@atlascopco.com>

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,12 +5,13 @@ import dash_html_components as html
 import dash_core_components as dcc
 import dash_daq as daq
 from datetime import datetime
+import functools
 import time
 import asyncio
 import json
 import os
 from quart import request
-from threading import Timer
+from threading import Lock, Timer
 
 app = dash_devices.Dash(__name__)
 app.config.suppress_callback_exceptions = True
@@ -24,8 +25,7 @@ def MetricCard(label, id):
     ], className="w3-container w3-cell w3-center w3-border-left")
 
 
-def TaskList(id):
-    columns = ["name", "identity", "queued", "started", "ended", "status", "worker"]
+def TaskList(id, columns=["worker", "name", "identity", "queued", "started", "ended", "status"]):
     return html.Div(dash_table.DataTable(
         id=id,
         columns=[{"name": i.title(), "id": i} for i in columns],
@@ -140,11 +140,11 @@ app.layout = html.Div([
             [
                 ("Overview", [
                     html.Div([
-                        daq.Gauge(id='metric_cluster_load', label="Cluster Load", value=0, max=0, scale={'start': 0, 'interval': 1, 'labelInterval': 2}),
+                        daq.Gauge(id='metric_cluster_load', label="Cluster Load", value=0, max=0, scale={'start': 0, 'interval': 1, 'labelInterval': '5'}),
                     ], className="w3-container w3-padding"),
                 ]),
                 ("Tasks", SubTabs([
-                    ("Live 100", TaskList(id="tasklist_live")),
+                    ("Live", TaskList(id="tasklist_live", columns=["worker", "name", "identity", "queued", "started", "status"])),
                     ("Last Hour", TaskList(id="tasklist")),
                 ], id="tabs-tasks")),
                 ("Workers", WorkerList(id="workerlist"))
@@ -154,10 +154,36 @@ app.layout = html.Div([
 ], className="w3-light-gray")
 
 
+
+def deduplicate(func):
+    @functools.wraps(func)
+    def decorator(self, task):
+        if os.getenv("DEDUPLICATION", "false") in ["true", "1"]:
+            dbtask = self.tasks_identities.get(task["identity"])
+            if dbtask and dbtask["instance"] != task["instance"]:
+                return
+            self.tasks_identities[task["identity"]] = task
+        func(self, task)
+        if os.getenv("DEDUPLICATION", "false") in ["true", "1"]:
+            dbtask = self.tasks_identities.get(task["identity"])
+            if dbtask and dbtask.get("ended"):
+                del self.tasks_identities[task["identity"]]
+    return decorator
+
+
+def locked(func):
+    @functools.wraps(func)
+    def decorator(self, *args, **kwargs):
+        with self.lock:
+            return func(self, *args, **kwargs)
+    return decorator
+
+
 class Dashboard(object):
     def __init__(self, app):
         self.tasks = []
         self.tasks_index = {}
+        self.tasks_identities = {} # identities in progress
         self.workers = {}
 
         self.metric_queued = 0
@@ -169,6 +195,7 @@ class Dashboard(object):
         self.timeout = 60
         self.timer = Timer(self.timeout, self.prune_tasks)
         self.timer.start()
+        self.lock = Lock()
 
         # Start push timer
         self.push_requested = False
@@ -178,19 +205,20 @@ class Dashboard(object):
         @app.callback(Output('tasklist', 'data'),
               [Input('tabs-tasks', 'value')])
         def render_content(tab):
-            return self.tasks
+            return self.tasks_ended
 
 
     def time(self):
         return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     def find(self, task):
-        dbtask = self.tasks_index.get(task["uuid"])
+        dbtask = self.tasks_index.get(task["instance"])
         if not dbtask:
             dbtask = task
             self.tasks.insert(0, dbtask)
-            self.tasks_index[task["uuid"]] = dbtask
-        dbtask["worker"] = task["worker"]
+            self.tasks_index[task["instance"]] = dbtask
+        if not task["client"]:
+            dbtask["worker"] = task["hostname"]
         return dbtask
 
     @property
@@ -203,7 +231,16 @@ class Dashboard(object):
             return 0
         return sum(map(lambda w: 1 if w["task"] else 0, self.workers.values()))
 
+    def undeduplicate(self, task):
+        if os.getenv("DEDUPLICATION", "false") in ["true", "1"]:
+            try:
+                del self.tasks_identities[task["identity"]]
+            except:
+                pass
+
     def update_workers(self, task):
+        if "worker" not in task:
+            return
         worker = {}
         worker["name"] = task["worker"]
         if task.get("started") and not task.get("ended"):
@@ -218,15 +255,23 @@ class Dashboard(object):
         dbworker.update(worker)
         self.workers[task["worker"]] = dbworker
 
+    @locked
+    @deduplicate
     def post_queued(self, task):
         task = self.find(task)
+        if task.get("queued"):
+            return
         task["queued"] = self.time()
         task["status"] = "queued"
         self.metric_queued += 1
         self.push()
 
+    @locked
+    @deduplicate
     def post_started(self, task):
         task = self.find(task)
+        if task.get("started"):
+            return
         task["started"] = self.time()
         task["status"] = "running"
         if task.get("queued"):
@@ -237,9 +282,12 @@ class Dashboard(object):
         self.update_workers(task)
         self.push()
 
-
+    @locked
+    @deduplicate
     def post_finished(self, task):
         task = self.find(task)
+        if task.get("ended"):
+            return
         task["ended"] = self.time()
         task["status"] = "passed"
         if task.get("started"):
@@ -254,8 +302,12 @@ class Dashboard(object):
         self.update_workers(task)
         self.push()
 
+    @locked
+    @deduplicate
     def post_failed(self, task):
         task = self.find(task)
+        if task.get("ended"):
+            return
         task["ended"] = self.time()
         task["status"] = "failed"
         if task.get("started"):
@@ -274,6 +326,7 @@ class Dashboard(object):
     def push(self):
         self.push_requested = True
 
+    @locked
     def push_callback(self):
         self.push_timer = Timer(2, self.push_callback)
         self.push_timer.start()
@@ -298,11 +351,17 @@ class Dashboard(object):
     def tasks_live(self):
         return list(filter(lambda t: not t.get("ended"), self.tasks))
 
+    @property
+    def tasks_ended(self):
+        return list(filter(lambda t: t.get("ended"), self.tasks))
+
+    @locked
     def prune_tasks(self):
         alive = []
         for task in self.tasks:
             if not self.task_seen(task, 60*60): # 1h
-                del self.tasks_index[task["uuid"]]
+                del self.tasks_index[task["instance"]]
+                self.undeduplicate(task)
                 if task["status"] == "passed":
                     self.metric_completed -= 1
                 elif task["status"] == "failed":
@@ -323,28 +382,18 @@ class Dashboard(object):
 dashboard = Dashboard(app)
 
 
-@app.server.route('/api/v1/task/queued', methods=['POST'])
+@app.server.route('/api/v1/tasks', methods=['POST'])
 async def queued():
     data = await request.get_json()
-    dashboard.post_queued(data)
-    return ""
 
-@app.server.route('/api/v1/task/started', methods=['POST'])
-async def started():
-    data = await request.get_json()
-    dashboard.post_started(data)
-    return ""
-
-@app.server.route('/api/v1/task/finished', methods=['POST'])
-async def finished():
-    data = await request.get_json()
-    dashboard.post_finished(data)
-    return ""
-
-@app.server.route('/api/v1/task/failed', methods=['POST'])
-async def failed():
-    data = await request.get_json()
-    dashboard.post_failed(data)
+    if data["event"] == "queued":
+        dashboard.post_queued(data)
+    elif data["event"] == "started":
+        dashboard.post_started(data)
+    elif data["event"] == "failed":
+        dashboard.post_failed(data)
+    elif data["event"] == "finished":
+        dashboard.post_finished(data)
     return ""
 
 
